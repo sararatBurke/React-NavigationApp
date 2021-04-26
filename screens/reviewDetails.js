691f0f520e44abdbfc0037a2ddadcfb98cd1a83b
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-26 19:42:34 -0400

    first commit

---
+++ screens/reviewDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ReviewDetails(){
+    return(
+        <View>
+            <Text>ReviewDetails screen</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    }
+})
