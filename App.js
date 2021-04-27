4708402a350cca05774070a357c032bbac521cfd
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-27 14:00:57 -0400

    added local font

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+
+const getFonts = () => Font.loadAsync({
+    'Roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-medium' : require('./assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf')
+  
+}); 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
