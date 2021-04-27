bb95a2570b0bdff5e808bd46a122674050b7c61f
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-27 14:09:07 -0400

    setup font

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {usedState} from 'react';
 import * as Font from 'expo-font';
 import Home from './screens/home';
+import { AppLoading } from 'expo';
 
 const getFonts = () => Font.loadAsync({
     'Roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
@@ -10,7 +11,20 @@ const getFonts = () => Font.loadAsync({
 }); 
 
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  
+  if (fontsLoaded){
+    return (
+      <Home />
+    );
+  }
+  else{
+    return(
+      <AppLoading
+      startAsync={getFonts}
+      onFinish={() =>  setFontsLoaded(true) }
+      />
+    )
+  }
+ 
 }
