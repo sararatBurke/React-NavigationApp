a62d3e8f336ce0481ad30ecc30764b7227c135b9
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-27 15:53:33 -0400

    create navigate buttons on Home n Review page

--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 
-export default function Home() {
+export default function Home({ navigation }) {
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails')
+    }
    
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titileText}>Home Screen</Text>
+            <Button title="review detail" onPress={pressHandler}></Button>
         </View>
     );
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails(){
+export default function ReviewDetails({ navigation }){
+    const pressHandler = () => {
+        navigation.navigate('Home')
+    }
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.titileText}>ReviewDetails screen</Text>
+            <Button title="Home" onPress={pressHandler}></Button>
         </View>
     )
 }
